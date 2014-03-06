e04ed7fc1e4174a765fea82b13d569f148d9814b
Michal Czyz <michalczyz@gmail.com>
2014-03-07 00:15:01 +0100

    let's keep like this event this a little crazy - still there is consistency

--- app/services/api_info/workshops.rb
+++ app/services/api_info/workshops.rb
@@ -1,16 +1,7 @@
 class ApiInfo::Workshops
   class << self
     def data
-      workshops.map(&:to_hash)
-    end
-
-    def workshops
-      @workshops ||=
-          workshops_keys.collect { |workshop_id| ::Workshop.new(workshop_id) }
-    end
-
-    def workshops_keys
-      I18n.t('workshops').keys
+      Workshop.all_to_hash
     end
   end
 end
