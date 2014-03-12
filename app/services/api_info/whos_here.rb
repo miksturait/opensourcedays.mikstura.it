1103d013cbb73e691ca20157efbcd7b9d452fb22
Michal Czyz <michalczyz@gmail.com>
2014-03-12 19:11:22 +0100

    few update about organizers and the mobile api

--- app/services/api_info/whos_here.rb
+++ app/services/api_info/whos_here.rb
@@ -2,27 +2,50 @@ class ApiInfo::WhosHere
   class << self
     def data
       {
-          sponsors: [
-              {
-                  type: "gold",
-                  name: "Red Hat",
-                  avatar_url: "",
-                  description: [
-                      "one",
-                      "two"
-                  ],
-                  links: {
-                      world: "",
-                      facebook: "",
-                      twitter: "",
-                  }
-              }
-          ]
-
+          sponsors: prepare_collection(:sponsors),
+          media: prepare_collection(:media),
+          organizers: prepare_collection(:organizers)
       }
+    end
+
+    def prepare_collection(type)
+      collection = send(type)
+      collection.collect do |(code, link)|
+        {
+            avatar_url: image_url(type, code),
+            link: link
+        }
+      end
     end
 
     private
 
+    def sponsors
+      {
+          "microsoft_openness" => 'http://www.microsoft.com/en-us/openness/default.asp',
+          "selleo" => 'http://selleo.com/',
+          "travis" => 'http://travis-ci.com/',
+          "rekord" => 'http://www.rekord.com.pl/'
+      }
+    end
+
+    def media
+      {
+          'magazyn.programista' => 'http://programistamag.pl/',
+          'bb365info.png' => 'http://www.bb365.info/'
+      }
+    end
+
+    def organizers
+    {
+        'mikstura.it' => 'http://mikstura.it',
+        'ath' => 'http://info.ath.bielsko.pl/',
+        'ath.reset' => 'http://reset.ath.bielsko.pl/'
+    }
+    end
+
+    def image_url(type, code)
+      [I18n.t(:domain), "assets/#{type}/logo", "#{code}.png"].join("/")
+    end
   end
 end
